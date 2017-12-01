dfb6b2c2b90a7a87713cbe36adcb1e7991194b62
Ali Kafagy <alikafag@buffalo.edu>
2017-12-01 11:41:45 -0800

    Added trend content to facebook.py and added a content column in create_facebook.sql

--- SQL/create/create_facebook.sql
+++ SQL/create/create_facebook.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS facebook(
   id INT PRIMARY KEY AUTO_INCREMENT,
   username VARCHAR(30),
   trend VARCHAR(100),
+  content VARCHAR(300),
   link VARCHAR(300),
   loadtime TIMESTAMP,
   FOREIGN KEY (username) REFERENCES users(username)
--- retreival/facebook.py
+++ retreival/facebook.py
@@ -7,14 +7,15 @@ from selenium.webdriver.support import expected_conditions as EC
 
 # at the beginning:
 start_time = time.time()
-connection = pymysql.connect(user='root', password='abc123', host='127.0.0.1', db='trendy', cursorclass=pymysql.cursors.DictCursor, use_unicode=True, charset="utf8mb4")
+connection = pymysql.connect(user='root', password='abc123', host='127.0.0.1', db='trendy',
+                             cursorclass=pymysql.cursors.DictCursor, use_unicode=True, charset="utf8mb4")
 with connection.cursor() as cursor:
     cursor.execute("TRUNCATE TABLE facebook;")
 connection.commit()
 
 chromeOptions = Options()
 chromeOptions.add_argument("--headless")
-chromeOptions.add_experimental_option('prefs', {'geolocation': True})
+chromeOptions.add_experimental_option("prefs", {"profile.default_content_setting_values.notifications": 2, 'geolocation': True})
 driver = webdriver.Chrome(chrome_options=chromeOptions)
 wait = WebDriverWait(driver, 30)
 driver.get("https://facebook.com/")
@@ -40,32 +41,35 @@ wait.until(EC.visibility_of_element_located((By.XPATH, '//a[@data-position="seem
 driver.find_element_by_xpath('//a[@data-position="seemore"]').click()
 time.sleep(3)
 
-# Trends
-headlines = []
+# Retrieval Process
 wait.until(EC.presence_of_all_elements_located((By.XPATH, '//a[starts-with(@data-hovercard, "/pubcontent/trending/hovercard/?topic_id=")]')))
 retrieved = driver.find_elements_by_xpath('//a[starts-with(@data-hovercard, "/pubcontent/trending/hovercard/?topic_id=")]')
-for i in retrieved:
-    trends = str(i.find_element_by_tag_name('div').text).split('\n')[0]
-    headlines.append(trends)
+while not len(retrieved) == 10:
+    retrieved = driver.find_elements_by_xpath('//a[starts-with(@data-hovercard, "/pubcontent/trending/hovercard/?topic_id=")]')
 
-# Trends Links
+# Trends Parsing
+headlines = []
+texts = []
+for heads in retrieved:
+    headlines.append(str(heads.find_element_by_tag_name('div').text).split('\n')[0])
+for text in retrieved:
+    texts.append(text.find_element_by_tag_name('span').text)
+
+# Trends Links retrieval and parsing
 links = []
 wait.until(EC.presence_of_all_elements_located((By.XPATH, '//a[starts-with(@href, "/topic")]')))
 hrefs = driver.find_elements_by_xpath('//a[starts-with(@href, "/topic")]')
 for href in hrefs:
     links.append(href.get_attribute('href'))
 
-for num in range(0, 9):
-        print(headlines[num] + " : " + links[num])
-
 username = 'alibaba'
 with connection.cursor() as cursor:
-    for num in range(0, 9):
-        print(headlines[num] + " : " + links[num])
-        sql = "INSERT INTO trendy.facebook(id, username, trend, link, loadtime) VALUES (DEFAULT, %s, %s, %s, NOW())"
-        cursor.execute(sql, (username, headlines[num], links[num]))
+    for num in range(0, len(headlines)):
+        print(headlines[num] + " : " + texts[num] + " : " + links[num])
+        sql = "INSERT INTO trendy.facebook(id, username, trend, content, link, loadtime) VALUES (DEFAULT, %s, %s, %s, %s, NOW())"
+        cursor.execute(sql, (username, headlines[num], texts[num], links[num]))
     connection.commit()
 
-
 # At the end of the program:
+driver.quit()
 print("Retrieval time took %f seconds" % (time.time() - start_time))
